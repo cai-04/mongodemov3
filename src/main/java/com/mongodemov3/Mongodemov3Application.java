8ac5de70a9a63bb3e241a394fd37e7bd3ba68f22
cai-04 <2387717648@qq.com>
2023-01-20 20:48:12 +0800

    Update Mongodemov3Application.java

--- src/main/java/com/mongodemov3/Mongodemov3Application.java
+++ src/main/java/com/mongodemov3/Mongodemov3Application.java
@@ -35,50 +35,6 @@ package com.mongodemov3;
  * */
 
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Mongodemov3Application {
-
-    public static void main(String[] args) {
-      package com.mongodemov3;
-/**
- * 版本v3
- * 2022/11/22 23:08
- * springboot+mongo电商系统
- * 模块实现：日志存储，订单管理，用户信息管理，历史订单
- * <p>
- * 版本v4
- * 2022/11/24 17:35
- * springboot+mongo电商系统
- * 模块实现：日志存储，订单管理，用户信息管理，历史订单
- * 修改内容：将service层拆分为dao层+service层，修改用户日志存储,日志控制台彩色打印输出
- * <p>
- * 版本v5
- * 2022/11-26 18：29
- * springboot+mongo电商系统
- * 模块实现：日志存储，订单管理，用户信息管理，历史订单
- * 修改内容：实现了通过id查找用户，修改了部分函数名称
- */
-
-/**
- * 版本v4
- * 2022/11/24 17:35
- * springboot+mongo电商系统
- * 模块实现：日志存储，订单管理，用户信息管理，历史订单
- * 修改内容：将service层拆分为dao层+service层，修改用户日志存储,日志控制台彩色打印输出
- * */
-
-/**
- * 版本v5
- * 2022/11-26 18：29
- * springboot+mongo电商系统
- * 模块实现：日志存储，订单管理，用户信息管理，历史订单
- * 修改内容：实现了通过id查找用户，修改了部分函数名称
- * */
-
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -95,17 +51,6 @@ public class Mongodemov3Application {
 
     public static void main(String[] args) throws UnknownHostException {
         ConfigurableApplicationContext application = SpringApplication.run(Mongodemov3Application.class, args);
-        log.info(" .----------------.  .----------------.  .----------------.  .----------------. \n" +
-                "| .--------------. || .--------------. || .--------------. || .--------------. |\n" +
-                "| |     ______   | || |      __      | || |     _____    | || |   _    _     | |\n" +
-                "| |   .' ___  |  | || |     /  \\     | || |    |_   _|   | || |  | |  | |    | |\n" +
-                "| |  / .'   \\_|  | || |    / /\\ \\    | || |      | |     | || |  | |__| |_   | |\n" +
-                "| |  | |         | || |   / ____ \\   | || |      | |     | || |  |____   _|  | |\n" +
-                "| |  \\ `.___.'\\  | || | _/ /    \\ \\_ | || |     _| |_    | || |      _| |_   | |\n" +
-                "| |   `._____.'  | || ||____|  |____|| || |    |_____|   | || |     |_____|  | |\n" +
-                "| |              | || |              | || |              | || |              | |\n" +
-                "| '--------------' || '--------------' || '--------------' || '--------------' |\n" +
-                " '----------------'  '----------------'  '----------------'  '----------------' ");
         Environment env = application.getEnvironment();
         String port = env.getProperty("server.port");
         String path1 = env.getProperty("server.servlet.context-path");
@@ -122,10 +67,6 @@ public class Mongodemov3Application {
                 "Local跳过登陆页面访问网址: \t\thttp://localhost:" + port + "/" + path1 + "\n\t" +
                 "Knife4j文档地址：\thttp://localhost" +  ":" + port + "/" + path2 + "\n\t" +
                 "----------------------------------------------------------");
-    }
-
-}
-
     }
 
 }
